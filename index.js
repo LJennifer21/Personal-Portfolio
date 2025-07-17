238ca00e2382edbab7361c5d9436902738c2ad91
LJennifer21 <lucerojennifer607@gmail.com>
2025-07-16 22:32:58 -0700

    Fixed issue with toggle

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let themeButton = document.getElementById("theme-button");
 
 const applysavetheme = () =>{
-    const savetheme  = localStorage.getItem("theme");
+    const savetheme  = localStorage.getItem("light-mode");//storing light mode
     if (savetheme){
-        document.body.classList.add(savetheme)
+        document.body.classList.add(savetheme);
+        
     }else{
-        document.body.classList.add("light-mode");
+        document.body.classList.add("dark-mode");//defult dark mode
     }
 }
 
@@ -13,15 +14,17 @@ const toggleLightMode = () => {
     if (document.body.classList.contains("light-mode")){//when light mode is on 
         document.body.classList.remove("light-mode");//remove light mode
         document.body.classList.add("dark-mode");//add dark mode
-        localStorage.setItem("theme","dark-mode");//stores dark mode
+        
         themeButton.textContent = "Light Mode";//button will say light mode
+        localStorage.setItem("theme","dark-mode");//stores dark mode
 
     }
     else{
         document.body.classList.remove("dark-mode");//when dark mode remove dark mode
         document.body.classList.add("light-mode");//add in light mode
-        localStorage.setItem("theme","light-mode");//set as light mode as the theme
+        
         themeButton.textContent="Dark Mode";//button will say dark mode 
+        localStorage.setItem("theme","light-mode");//set as light mode as the theme
     }
 }
 themeButton.addEventListener("click", toggleLightMode);//click to go from light to dark or dark to light mode
